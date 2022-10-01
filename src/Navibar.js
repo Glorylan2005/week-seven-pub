7310ee439699ccb24c7457473c7c506ed8143cd4
Natali Lohr <nlohr@lohrconstructionco.com>
2022-10-01 19:43:18 -0400

    third commit

--- src/Navibar.js
+++ src/Navibar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Nav, Navbar } from "react-bootstrap";
@@ -8,19 +9,22 @@ import Logo from "./whitecat3.png";
 
 
 function Navibar() {
+    
+    const [expanded, setExpanded] = useState(false);
+
     return (
-    <Navbar fixed="top" collapseOnSelect expand="md" >
+    <Navbar expanded={expanded} fixed="top" collapseOnSelect expand="md" >
         <Navbar.Brand>
         <img className="logoimg" src={Logo} width='120px' alt="logo" />
         </Navbar.Brand>
 
-        <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")}  aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-2">
-                <Nav.Link><Link to="/" className='link'>HOME</Link></Nav.Link>
-                <Nav.Link><Link to="/about" className='link'>ABOUT</Link></Nav.Link>
-                <Nav.Link><Link to="/menu" className='link redmenu'>MENU</Link></Nav.Link>
-                <Nav.Link><Link to="/contactus" className='link'>CONTACT US</Link></Nav.Link>
+                <Nav.Link><Link onClick={() => setExpanded(false)} to="/" className='link'>HOME</Link></Nav.Link>
+                <Nav.Link><Link onClick={() => setExpanded(false)} to="/about" className='link'>ABOUT</Link></Nav.Link>
+                <Nav.Link><Link onClick={() => setExpanded(false)} to="/menu" className='link redmenu'>MENU</Link></Nav.Link>
+                <Nav.Link><Link onClick={() => setExpanded(false)} to="/contactus" className='link'>CONTACT US</Link></Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
